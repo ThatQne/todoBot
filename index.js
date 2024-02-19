29ac5c6690b68e55d3f2eea70ddfe901793e3350
ThatQne <82828236+ThatQne@users.noreply.github.com>
2024-02-18 22:13:25 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,170 +1,146 @@
-(async () => {
-  // default imports
-  const events = require("events");
-  const { exec } = require("child_process");
-  const logs = require("discord-logs");
-  const Discord = require("discord.js");
-  const {
-    MessageEmbed,
-    MessageButton,
-    MessageActionRow,
-    Intents,
-    Permissions,
-    MessageSelectMenu,
-  } = require("discord.js");
-  const fs = require("fs");
-  let process = require("process");
-  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+(async()=>{
+    // default imports
+    const events = require('events');
+    const { exec } = require("child_process")
+    const logs = require("discord-logs")
+    const Discord = require("discord.js")
+    const { 
+        MessageEmbed, 
+        MessageButton, 
+        MessageActionRow, 
+        Intents, 
+        Permissions, 
+        MessageSelectMenu 
+    }= require("discord.js")
+    const fs = require('fs');
+    let process = require('process');
+    const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-  // block imports
-  const ms = require("ms");
-  let https = require("https");
+    // block imports
+    let https = require("https")
+    
+    // define s4d components (pretty sure 90% of these arnt even used/required)
+    let s4d = {
+        Discord,
+        fire:null,
+        joiningMember:null,
+        reply:null,
+        player:null,
+        manager:null,
+        Inviter:null,
+        message:null,
+        notifer:null,
+        checkMessageExists() {
+            if (!s4d.client) throw new Error('You cannot perform message operations without a Discord.js client')
+            if (!s4d.client.readyTimestamp) throw new Error('You cannot perform message operations while the bot is not connected to the Discord API')
+        }
+    };
 
-  // define s4d components (pretty sure 90% of these arnt even used/required)
-  let s4d = {
-    Discord,
-    fire: null,
-    joiningMember: null,
-    reply: null,
-    player: null,
-    manager: null,
-    Inviter: null,
-    message: null,
-    notifer: null,
-    checkMessageExists() {
-      if (!s4d.client)
-        throw new Error(
-          "You cannot perform message operations without a Discord.js client",
-        );
-      if (!s4d.client.readyTimestamp)
-        throw new Error(
-          "You cannot perform message operations while the bot is not connected to the Discord API",
-        );
-    },
-  };
+    // check if d.js is v13
+    if (!require('./package.json').dependencies['discord.js'].startsWith("^13.")) {
+      let file = JSON.parse(fs.readFileSync('package.json'))
+      file.dependencies['discord.js'] = '^13.16.0'
+      fs.writeFileSync('package.json', JSON.stringify(file, null, 4))
+      exec('npm i')
+      throw new Error("Seems you arent using v13 please re-run or run `npm i discord.js@13.16.0`");
+    }
 
-  // check if d.js is v13
-  if (
-    !require("./package.json").dependencies["discord.js"].startsWith("^13.")
-  ) {
-    let file = JSON.parse(fs.readFileSync("package.json"));
-    file.dependencies["discord.js"] = "^13.16.0";
-    fs.writeFileSync("package.json", JSON.stringify(file, null, 4));
-    exec("npm i");
-    throw new Error(
-      "Seems you arent using v13 please re-run or run `npm i discord.js@13.16.0`",
-    );
-  }
+    // check if discord-logs is v2
+    if (!require('./package.json').dependencies['discord-logs'].startsWith("^2.")) {
+      let file = JSON.parse(fs.readFileSync('package.json'))
+      file.dependencies['discord-logs'] = '^2.0.0'
+      fs.writeFileSync('package.json', JSON.stringify(file, null, 4))
+      exec('npm i')
+      throw new Error("discord-logs must be 2.0.0. please re-run or if that fails run `npm i discord-logs@2.0.0` then re-run");
+    }
 
-  // check if discord-logs is v2
-  if (
-    !require("./package.json").dependencies["discord-logs"].startsWith("^2.")
-  ) {
-    let file = JSON.parse(fs.readFileSync("package.json"));
-    file.dependencies["discord-logs"] = "^2.0.0";
-    fs.writeFileSync("package.json", JSON.stringify(file, null, 4));
-    exec("npm i");
-    throw new Error(
-      "discord-logs must be 2.0.0. please re-run or if that fails run `npm i discord-logs@2.0.0` then re-run",
-    );
-  }
-
-  // create a new discord client
-  s4d.client = new s4d.Discord.Client({
-    intents: [
-      Object.values(s4d.Discord.Intents.FLAGS).reduce((acc, p) => acc | p, 0),
-    ],
-    partials: ["REACTION", "CHANNEL"],
-  });
-
-  // when the bot is connected say so
-  s4d.client.on("ready", () => {
-    console.log(s4d.client.user.tag + " is alive!");
-  });
-
-  // upon error print "Error!" and the error
-  process.on("uncaughtException", function (err) {
-    console.log("Error!");
-    console.log(err);
-  });
-
-  // give the new client to discord-logs
-  logs(s4d.client);
-
-  // pre blockly code
-
-  // blockly code
-  var tempMessage;
-
-  await s4d.client
-    .login(
-      "MTIwODgxNTc5MjI4MDMwNTgwNQ.G3g0Wd.eAXgjBaXFtB4IpvyV9MXNH_Ngis4om3kWFtqJU",
-    )
-    .catch((e) => {
-      const tokenInvalid = true;
-      const tokenError = e;
-      if (e.toString().toLowerCase().includes("token")) {
-        throw new Error("An invalid bot token was provided!");
-      } else {
-        throw new Error(
-          "Privileged Gateway Intents are not enabled! Please go to https://discord.com/developers and turn on all of them.",
-        );
-      }
+    // create a new discord client
+    s4d.client = new s4d.Discord.Client({
+        intents: [
+            Object.values(s4d.Discord.Intents.FLAGS).reduce((acc, p) => acc | p, 0)
+        ],
+        partials: [
+            "REACTION", 
+            "CHANNEL"
+        ]
     });
 
-  const http = require("http");
-  const server = http.createServer((req, res) => {
-    res.writeHead(200);
-    res.end("This site was created to keep bot on 25/8");
-  });
-  server.listen(3000);
+    // when the bot is connected say so
+    s4d.client.on('ready', () => {
+        console.log(s4d.client.user.tag + " is alive!")
+    })
 
-  s4d.client.on("messageCreate", async (s4dmessage) => {
-    if (
-      String(s4dmessage.channel.name).includes(String("todo")) ||
-      String(s4dmessage.channel.name).includes(String("checklist"))
-    ) {
-      if (s4dmessage.author.bot) {
-        s4dmessage.react("✅");
+    // upon error print "Error!" and the error
+    process.on('uncaughtException', function (err) {
+        console.log('Error!');
+        console.log(err);
+    });
+
+    // give the new client to discord-logs
+    logs(s4d.client);
+
+    // pre blockly code
+    
+
+    // blockly code
+    var tempMessage;
+    
+    
+    await s4d.client.login((process.env[String('TOKEN')])).catch((e) => {
+            const tokenInvalid = true;
+            const tokenError = e;
+            if (e.toString().toLowerCase().includes("token")) {
+                throw new Error("An invalid bot token was provided!")
+            } else {
+                throw new Error("Privileged Gateway Intents are not enabled! Please go to https://discord.com/developers and turn on all of them.")
+            }
+        });
+    
+    const http = require('http');
+    const server = http.createServer((req, res) => {
+        res.writeHead(200);
+        res.end('This site was created to keep bot on 25/8');
+    });
+    server.listen(3000);
+    
+    s4d.client.on('messageCreate', async (s4dmessage) => {
+      if ((String(((s4dmessage.channel).name)).includes(String('todo'))) || (String(((s4dmessage.channel).name)).includes(String('checklist')))) {
+        if ((s4dmessage.author).bot) {
+          (s4dmessage).react('✅')
+              } else {
+          tempMessage = (s4dmessage.content);
+          (s4dmessage).delete()
+              s4dmessage.channel.send(tempMessage);
+        }
       } else {
-        tempMessage = s4dmessage.content;
-        s4dmessage.delete();
-        s4dmessage.channel.send(tempMessage);
+        (s4dmessage.guild).channels.create('todo', {
+                type: 'text'
+            });
       }
-    } else {
-      s4dmessage.guild.channels.create("todo", {
-        type: "text",
-      });
-    }
-  });
-
-  s4d.client.on("messageReactionAdd", async (reaction, user) => {
-    if (
-      String(reaction.message.channel.name).includes(String("todo")) ||
-      String(reaction.message.channel.name).includes(String("checklist"))
-    ) {
-      if (!user.bot) {
-        tempMessage = reaction.message;
-        reaction.message.edit(String("**COMPLETE: **" + String(tempMessage)));
+    
+    });
+    
+    s4d.client.on('messageReactionAdd', async (reaction,user) => {
+      if ((String(((reaction.message.channel).name)).includes(String('todo'))) || (String(((reaction.message.channel).name)).includes(String('checklist')))) {
+        if (!((user).bot)) {
+          tempMessage = (reaction.message);
+          (reaction.message).edit(String(('**COMPLETE** || ' + String(tempMessage))))
+              }
       }
-    }
-  });
-
-  s4d.client.on("messageReactionRemove", async (reaction, user) => {
-    if (
-      String(reaction.message.channel.name).includes(String("todo")) ||
-      String(reaction.message.channel.name).includes(String("checklist"))
-    ) {
-      if (!user.bot) {
-        tempMessage = String(reaction.message).replaceAll(
-          "**COMPLETE: **",
-          String(" "),
-        );
-        tempMessage = tempMessage.trim();
-        reaction.message.edit(String(tempMessage));
+    
+    });
+    
+    s4d.client.on('messageReactionRemove', async (reaction,user) => {
+      if ((String(((reaction.message.channel).name)).includes(String('todo'))) || (String(((reaction.message.channel).name)).includes(String('checklist')))) {
+        if (!((user).bot)) {
+          tempMessage = (reaction.message);
+          tempMessage = (String(tempMessage).replaceAll('**COMPLETE** || ', String(' ')));
+          tempMessage = tempMessage.trim();
+          (reaction.message).edit(String(tempMessage))
+              }
       }
-    }
-  });
-
-  return s4d;
+    
+    });
+    
+    return s4d
 })();
